822b4f6e839763ad1cf75b8d3cde5a5f317ba5a0
Anand Kumar Jha <jha.anandjha.anand24@gmail.com>
2019-12-28 18:25:51 +0530

    Local broadcast reciever

--- app/src/main/java/com/example/myexperiments/broadcast_reciever/BroadcastTestingActivity.kt
+++ app/src/main/java/com/example/myexperiments/broadcast_reciever/BroadcastTestingActivity.kt
@@ -1,12 +1,15 @@
 package com.example.myexperiments.broadcast_reciever
 
+import android.content.BroadcastReceiver
+import android.content.Context
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.content.IntentFilter
 import com.example.myexperiments.R
 import kotlinx.android.synthetic.main.activity_broadcast_testing.*
 import android.content.Intent
-
+import android.util.Log
+import androidx.localbroadcastmanager.content.LocalBroadcastManager
 
 
 class BroadcastTestingActivity : AppCompatActivity() {
@@ -21,19 +24,36 @@ class BroadcastTestingActivity : AppCompatActivity() {
         receiver = MyBroadcastReciever()
         intentFilter = IntentFilter("com.journaldev.broadcastreceiver.SOME_ACTION")
 
-        btn_fire_intent.setOnClickListener{
+        btn_fire_intent.setOnClickListener {
             val intent = Intent("com.journaldev.broadcastreceiver.SOME_ACTION")
             sendBroadcast(intent)
+        }
+
+        btn_local_broadcast_reciever.setOnClickListener{
+            //local broadcast action fire
+            LocalBroadcastManager.getInstance(this@BroadcastTestingActivity).sendBroadcast(Intent("custom_action"))
         }
     }
 
     override fun onResume() {
         super.onResume()
         registerReceiver(receiver, intentFilter)
+
+        //register local broadcast reciever
+        LocalBroadcastManager.getInstance(this@BroadcastTestingActivity).registerReceiver(myBroadcastReciever, IntentFilter("custom_action"))
     }
 
     override fun onDestroy() {
         super.onDestroy()
-        registerReceiver(receiver, intentFilter)
+        unregisterReceiver(receiver)
+
+        //register local broadcast reciever
+        LocalBroadcastManager.getInstance(this@BroadcastTestingActivity).unregisterReceiver(myBroadcastReciever)
+    }
+
+    val myBroadcastReciever: BroadcastReceiver = object : BroadcastReceiver() {
+        override fun onReceive(p0: Context?, p1: Intent?) {
+            Log.e("TAG", "BroadCast recieved")
+        }
     }
 }
