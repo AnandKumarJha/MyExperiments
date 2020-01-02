3e5eb2213504019b6be4c5c0ad3a720daf206dcc
Anand Kumar Jha <jha.anandjha.anand24@gmail.com>
2020-01-02 12:31:03 +0530

    module testing done

---
+++ app/src/main/java/com/example/myexperiments/module_parent/ModuleParentActivity.kt
@@ -0,0 +1,12 @@
+package com.example.myexperiments.module_parent
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class ModuleParentActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_module_parent)
+    }
+}
---
+++ commonmodule/src/main/java/com/example/commonmodule/CommonTestingActivity.kt
@@ -0,0 +1,12 @@
+package com.example.commonmodule
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class CommonTestingActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_common_testing)
+    }
+}
